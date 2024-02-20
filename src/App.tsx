4742080562e433f4218ec9b084d34e7f993df894
gs18115 <eunsoo0327@naver.com>
2024-02-20 18:23:57 +0900

    msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import msg from '/msg.jpg'
 import './App.css'
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://game.mahjongsoul.com" target="_blank">
+          <img src={msg} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
